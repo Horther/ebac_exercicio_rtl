7d05d4a3998c85f3824556a8ea3f56b3c9a9989e
Horther <77111164+Horther@users.noreply.github.com>
2026-03-06 12:10:39 +0000

    Testando os comentários
    testando a inserção de 2 comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,25 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Conter dois comentários após o teste', () => {
+    render(<PostComment/>);
+
+    fireEvent.change(screen.getByTestId('comTextArea'), {
+        target: {
+            value: 'Adicionado o 1 cementario'
+        }
+    });
+    fireEvent.click(screen.getByTestId('comButton'))
+
+    fireEvent.change(screen.getByTestId('comTextArea'), {
+        target: {
+            value: 'Adicionado o 2 cementario'
+        }
+    });
+    fireEvent.click(screen.getByTestId('comButton'))
+
+    expect(screen.getAllByTestId('comElem')).toHaveLength(2);
 });
+});
+
